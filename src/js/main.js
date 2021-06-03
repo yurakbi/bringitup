85384ec4d9ca5e4763a83bd9b1a3b2701aedf2b2
yura <yura04091@gmail.com>
2021-06-03 21:02:52 +0300

    slider-mini

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,37 @@
 import MainSlider from './modules/slider/slider-main';
+import MiniSlider from './modules/slider/slider-mini';
 import  VideoPlayer from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new MainSlider({btns: '.next', page: '.page'});
+    const slider = new MainSlider({btns: '.next', container: '.page'});
     slider.render();
+
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
+        next: '.showup__next',
+        prev: '.showup__prev',
+        activeClass: 'card-active',
+        animate: true
+    });
+    showUpSlider.init();
+
+    const modulesSlider = new MiniSlider ({
+        container: '.modules__content-slider',
+        next: '.modules__info-btns .slick-next',
+        prev: '.modules__info-btns .slick-prev',
+        activeClass: 'card-active',
+        animate: true,
+        autoplay: true
+    });
+    modulesSlider.init();
+
+    const feedSlider = new MiniSlider ({
+        container: '.feed__slider',
+        next: '.feed__slider .slick-next',
+        prev: '.feed__slider .slick-prev',
+        activeClass: 'feed__item-active'
+    });
+    feedSlider.init();
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
